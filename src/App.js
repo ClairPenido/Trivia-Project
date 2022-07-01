44b998d32eb160a2018e7a9b79c5593a6b88a7a3
Heloisa <heloisajacobi@gmail.com>
2022-07-01 17:53:46 -0300

    Requisito_13

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Game from './pages/Game';
-import Feedback from './pages/Feedback';
 import Configuracao from './pages/Configuracao';
+import Feedback from './pages/Feedback';
 
 export default function App() {
   return (
     <div>
       <Route exact path="/" component={ Login } />
       <Route path="/game" component={ Game } />
-      <Route path="/feedback" component={ Feedback } />
       <Route path="/configuracao" component={ Configuracao } />
+      <Route path="/feedback" component={ Feedback } />
     </div>
   );
 }
--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Header from '../components/Header';
 
 class Feedback extends Component {
+  message = () => {
+    const { assertions } = this.props;
+    const point = 3;
+    console.log(assertions);
+    return assertions <= point ? 'Could be better...' : 'Well Done!';
+  };
+
   render() {
     return (
       <div>
         <Header />
-        <h1>Feedback</h1>
+        <h3 data-testid="feedback-text">
+          { this.message() }
+        </h3>
       </div>
     );
   }
 }
 
-export default Feedback;
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  assertions: state.player.assertions });
+
+export default connect(mapStateToProps, null)(Feedback);
